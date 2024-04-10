c337a5b73a812c9c16c49b6ea94a001cf54a00cd
Wais A Almakaleh <wais.almakaleh@gmail.com>
2024-04-10 07:55:25 -0400

    fixed Javascript issue

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ function createIframe() {
   const iframe = document.createElement('iframe');
   iframe.id = 'myIframe';
   iframe.src = 'https://masjidbox.com/prayer-times/islamic-society-of-schuylkill-county';
-  iframe.scrolling = "no";
   return iframe;
 }
 
 function handleIframeVisibility() {
   const prayerTimeHomeDiv = document.querySelector('.iframe-container');
   const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	
-	
+
+
   if (screenWidth <= 700) {
     const iframe = createIframe();
     prayerTimeHomeDiv.appendChild(iframe);
@@ -25,6 +24,3 @@ console.log("this is taking forever");
 
 
 window.addEventListener('load', handleIframeVisibility);
-
-
- window.addEventListener('resize', handleIframeVisibility);
