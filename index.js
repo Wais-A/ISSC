7a2bbb61d682eec3060a8430a049360a7a359add
Wais A Almakaleh <wais.almakaleh@gmail.com>
2024-04-10 06:51:35 -0400

    initial index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+// initial
+// 
+// <script type="text/javascript" src="https://isscmasjid.org/wp-content/themes/ISSC/index.js"></script>
+// 
+// <iframe src="https://masjidbox.com/prayer-times/islamic-society-of-schuylkill-county" scrolling="no"></iframe>
+// 
+// 
+//   function createIframe() {
+//   const iframe = document.createElement('iframe');
+//   iframe.id = 'myIframe';
+//   iframe.src = 'https://www.example.com';
+//   return iframe;
+// }
+
+// function handleIframeVisibility() {
+//   const prayerTimeHomeDiv = document.querySelector('.prayerTimeHome');
+//   const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+//   if (screenWidth <= 700) {
+//     const iframe = createIframe();
+//     prayerTimeHomeDiv.appendChild(iframe);
+//   } else {
+//     const iframe = document.getElementById('myIframe');
+//     if (iframe) {
+//       prayerTimeHomeDiv.removeChild(iframe);
+//     }
+//   }
+// }
+
+// // Call the function on page load
+// window.addEventListener('load', handleIframeVisibility);
+
+// // Call the function on window resize
+// window.addEventListener('resize', handleIframeVisibility);
+
+console.log("Hi yall There!");
+
+
+
+function createIframe() {
+  const iframe = document.createElement('iframe');
+  iframe.id = 'myIframe';
+  iframe.src = 'https://masjidbox.com/prayer-times/islamic-society-of-schuylkill-county';
+  iframe.scrolling = "no";
+  return iframe;
+}
+
+
+
+function handleIframeVisibility() {
+  const prayerTimeHomeDiv = document.querySelector('.iframe-container');
+  const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	
+	
+  if (screenWidth <= 700) {
+    const iframe = createIframe();
+    prayerTimeHomeDiv.appendChild(iframe);
+  } else {
+    const iframe = document.getElementById('myIframe');
+    if (iframe) {
+      prayerTimeHomeDiv.removeChild(iframe);
+    }
+  }
+}
+
+// Call the function on page load
+window.addEventListener('load', handleIframeVisibility);
+
+// Call the function on window resize
+ window.addEventListener('resize', handleIframeVisibility);
