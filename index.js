30aa4c38253ac4345b2ff0bf158ddced0f7ecf99
Wais A Almakaleh <wais.almakaleh@gmail.com>
2024-04-17 19:00:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
-let iframeBigMode = true;
+const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+const iframeContainer = document.getElementsByClassName("iframe-container");
+const iframeElement = document.createElement("iframe");
+
+
+function makeIframe(n) {
+  iframeContainer[n].append(iframeElement);
+  iframeElement.title = "prayer times";
+  iframeElement.src = 'https://masjidbox.com/prayer-times/islamic-society-of-schuylkill-county';
+  iframeElement.scrolling = 'no';
+  iframeContainer[n].style.height = "600px";
+}
 
 function handleIframeVisibility() {
-  const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  const iframeContainer = document.getElementsByClassName("iframe-container");
-  const iframeElement = document.createElement("iframe");
-
-
-
-  function makeIframe(n) {
-    iframeContainer[n].append(iframeElement);
-    iframeElement.title = "prayer times";
-    iframeElement.src = 'https://masjidbox.com/prayer-times/islamic-society-of-schuylkill-county';
-    iframeElement.scrolling = 'no';
-    iframeContainer[n].style.height = "600px";
-  }
-
-  function removeIframe(n) {
-    iframeContainer[n].style.height = "0px";
-    iframeContainer[n].firstElementChild.src = '';
-  }
-
   if (screenWidth <= 1024) {
-    if (iframeBigMode) {
-      removeIframe(0);
-      iframeBigMode = false;
-    }
     makeIframe(1);
   } else {
-    if (!iframeBigMode) {
-      removeIframe(1);
-      iframeBigMode = true;
-    }
     makeIframe(0);
   }
 }
 
+function checkIframe(n) {
+  const doesExist = iframeContainer[n].firstElementChild;
+  return doesExist;
+}
 
-window.addEventListener('resize', handleIframeVisibility);
+function handleIframeVisibilityOnResize() {
+
+  function removeIframe(n) {
+    iframeContainer[n].removeChild(iframeContainer[n].firstElementChild);
+    iframeContainer[n].style.height = "0px";
+  }
+
+  if (screenWidth <= 1024) {
+    makeIframe(1);
+    if (checkIframe(0)) {
+      removeIframe(0);
+    }
+  } else if (screenWidth >= 1024) {
+    makeIframe(0);
+    if (checkIframe(1)) {
+      removeIframe(1);
+    }
+  }
+}
+
 window.addEventListener('load', handleIframeVisibility);
+window.addEventListener('resize', handleIframeVisibilityOnResize);
