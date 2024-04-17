7c8cae339698a31b3e88fe2b51cd99cb805286af
Wais A Almakaleh <wais.almakaleh@gmail.com>
2024-04-17 19:45:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,45 +4,21 @@ const iframeElement = document.createElement("iframe");
 
 
 function makeIframe(n) {
-  iframeContainer[n].append(iframeElement);
   iframeElement.title = "prayer times";
   iframeElement.src = 'https://masjidbox.com/prayer-times/islamic-society-of-schuylkill-county';
   iframeElement.scrolling = 'no';
   iframeContainer[n].style.height = "600px";
+  iframeContainer[n].append(iframeElement);
 }
 
 function handleIframeVisibility() {
-  if (screenWidth <= 1024) {
+  if (screenWidth <= 1400) {
     makeIframe(1);
   } else {
     makeIframe(0);
   }
 }
 
-function checkIframe(n) {
-  const doesExist = iframeContainer[n].firstElementChild;
-  return doesExist;
-}
-
-function handleIframeVisibilityOnResize() {
-
-  function removeIframe(n) {
-    iframeContainer[n].removeChild(iframeContainer[n].firstElementChild);
-    iframeContainer[n].style.height = "0px";
-  }
-
-  if (screenWidth <= 1024) {
-    makeIframe(1);
-    if (checkIframe(0)) {
-      removeIframe(0);
-    }
-  } else if (screenWidth >= 1024) {
-    makeIframe(0);
-    if (checkIframe(1)) {
-      removeIframe(1);
-    }
-  }
-}
 
 window.addEventListener('load', handleIframeVisibility);
-window.addEventListener('resize', handleIframeVisibilityOnResize);
+window.addEventListener('resize', handleIframeVisibility);
